510eaee61b9a8561ad91a975191952243b43f6e7
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2026-06-07 15:12:37 +0200

    change default branch

--- scripts/commit_version.sh
+++ scripts/commit_version.sh
@@ -12,7 +12,7 @@ if git diff --staged --quiet; then
     echo "latest_version.txt already contains ${VERSION}, nothing to commit"
 else
     git commit -m "ci: update to ${VERSION}"
-    git push origin "${BRANCH_NAME}"
+    git push origin main
     echo "Pushed latest_version.txt to origin/${BRANCH_NAME}"
 fi
 
