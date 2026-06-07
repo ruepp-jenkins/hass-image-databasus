fe022a9cef657faef61a58bcd2d8724fb02081a2
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2026-06-07 14:52:02 +0200

    -

--- scripts/commit_version.sh
+++ scripts/commit_version.sh
@@ -4,14 +4,13 @@ set -e
 echo "${VERSION}" > latest_version.txt
 echo "Updated latest_version.txt to ${VERSION}"
 
-#git config user.email "jenkins@noreply.ruepp.info"
-#git config user.name "Jenkins"
 git add latest_version.txt
 
 if git diff --staged --quiet; then
     echo "latest_version.txt already contains ${VERSION}, nothing to commit"
 else
-    git commit -m "[skip ci] Update latest_version.txt to ${VERSION}"
+    git commit -m "ci: update to ${VERSION}"
     git push origin "${BRANCH_NAME}"
     echo "Pushed latest_version.txt to origin/${BRANCH_NAME}"
 fi
+
